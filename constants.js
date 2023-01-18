fd3b3e21eab99c16600b29b4a3e1568e8a9cd95b
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-18 15:46:46 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const MOVE_UP = 'w';
+const MOVE_LEFT = 'a';
+const MOVE_DOWN = 's';
+const MOVE_RIGHT = 'd';
+const NAME = 'AVG';
+const MESSAGES =
+  {x: 'Say: YOLO',
+    c: 'Say: Try Harder',
+    z: 'Say: Got You'};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  NAME,
+  MESSAGES
+};
+
